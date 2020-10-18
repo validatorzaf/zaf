b2c18fa872b3dfb72b8ca3230b0c9bf244d11d60
Petr Pytelka <petr.pytelka@lightcomp.cz>
2020-10-19 00:38:08 +0200

    Oprava pravidla 99 - spousti se jen pro original ve vystupnnim formatu

--- src/zaf-core/src/main/java/cz/zaf/sipvalidator/nsesss2017/JmenaElementu.java
+++ src/zaf-core/src/main/java/cz/zaf/sipvalidator/nsesss2017/JmenaElementu.java
@@ -4,4 +4,9 @@ public final class JmenaElementu {
     public final static String DIL = "nsesss:Dil";
     public final static String SPIS = "nsesss:Spis";
     public final static String EVIDENCNI_UDAJE = "nsesss:EvidencniUdaje";
+
+    public final static String FORMA_UCHOVANI = "forma_uchovani";
+    public final static String FORMA_UCHOVANI_ORIGINAL_VE_VYST_DAT_FORMATU = "originál ve výstupním datovém formátu";
+    public static final String DMDID = "DMDID";
+    public final static String ID = "ID";
 }
--- src/zaf-core/src/main/java/cz/zaf/sipvalidator/nsesss2017/pravidla06/obs90_99/Pravidlo99.java
+++ src/zaf-core/src/main/java/cz/zaf/sipvalidator/nsesss2017/pravidla06/obs90_99/Pravidlo99.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.apache.commons.lang3.StringUtils;
 import org.verapdf.core.EncryptedPdfException;
 import org.verapdf.core.ModelParsingException;
 import org.verapdf.core.ValidationException;
@@ -17,6 +20,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import cz.zaf.sipvalidator.helper.HelperString;
+import cz.zaf.sipvalidator.nsesss2017.JmenaElementu;
 import cz.zaf.sipvalidator.nsesss2017.K06PravidloBase;
 import cz.zaf.sipvalidator.nsesss2017.K06_Obsahova;
 import cz.zaf.sipvalidator.nsesss2017.ValuesGetter;
@@ -39,49 +43,88 @@ public class Pravidlo99  extends K06PravidloBase {
 
 	@Override
 	protected boolean kontrolaPravidla() {
-		
+
+        // získání všech komponent ve výstupním datovém formátu
+        NodeList komponenty = ValuesGetter.getAllAnywhere("nsesss:Komponenta", metsParser.getDocument());
+        if (komponenty == null || komponenty.getLength() == 0) {
+            return true;
+        }
+
+        Set<String> checkFiles = new HashSet<>();
+        for (int ki = 0; ki < komponenty.getLength(); ki++) {
+            Node komponenta = komponenty.item(ki);
+            String formaUchovani = ValuesGetter.getValueOfAttribut(komponenta, JmenaElementu.FORMA_UCHOVANI);
+            if (JmenaElementu.FORMA_UCHOVANI_ORIGINAL_VE_VYST_DAT_FORMATU.equals(formaUchovani)) {
+                String id = ValuesGetter.getValueOfAttribut(komponenta, JmenaElementu.ID);
+                if (StringUtils.isEmpty(id)) {
+                    return nastavChybu("Komponenta neni ve formátu Pdf/A (chybí atribut ID) " + komponenta,
+                                       getMistoChyby(komponenta));
+                }
+                checkFiles.add(id);
+            }
+        }
+
 		NodeList nodeListFileGrp = ValuesGetter.getAllAnywhere("mets:fileGrp", metsParser.getDocument());
 		if (nodeListFileGrp==null) {
 			return true;
 		}
 		for(int fg=0;fg<nodeListFileGrp.getLength();fg++) {
 			Node fileGrpNode = nodeListFileGrp.item(fg);
-			if ("Original".equals(ValuesGetter.getValueOfAttribut(fileGrpNode, "USE"))) {
-				ArrayList<Node> fileNodes = ValuesGetter.getChildList(fileGrpNode, "mets:file");
-				for (Node fileNode: fileNodes) {
-					String mimeType = ValuesGetter.getValueOfAttribut(fileNode, "MIMETYPE"); 
-					if ("application/pdf".equalsIgnoreCase(mimeType)) {
-						ArrayList<Node> flocatNodes = ValuesGetter.getChildList(fileNode, "mets:FLocat");
-						for(Node flocatNode:flocatNodes) {
-							if (ValuesGetter.hasAttribut(flocatNode, "xlink:href")) {
-				        		String href = ValuesGetter.getValueOfAttribut(flocatNode, "xlink:href");
-				                if (href.startsWith("komponenty")) {
-				                	href = HelperString.edit_path(href);
-				                	String cestaKeKomponente = SIP_MAIN_helper.getCesta_komponenty(context.getSip()).replaceFirst("komponenty",
-				                            "") + href;
-				                	File file = new File(cestaKeKomponente);
-				                	if (file.exists()) {
-				                		try (PDFAParser parser = Foundries.defaultInstance().createParser(new FileInputStream(file))) {
-				                		    PDFAValidator validator = Foundries.defaultInstance().createValidator(parser.getFlavour(), false);
-				                		    ValidationResult result = validator.validate(parser);
-				                		    if (!result.isCompliant()) {
-				                		    	return nastavChybu("Komponenta neni ve formátu Pdf/A " + cestaKeKomponente
-				                                        , getMistoChyby(flocatNode));
-				                		    }
-				                		} catch (ModelParsingException | EncryptedPdfException | IOException | ValidationException e) {
-											return false;
-										}
-				                	}
-				                }                
-				            }  
-						}
-					}
-				}				
-			}
+            ArrayList<Node> fileNodes = ValuesGetter.getChildList(fileGrpNode, "mets:file");
+            for (Node fileNode : fileNodes) {
+                // overeni ID, zda ma byt soubor kontrolovan
+                String dmdid = ValuesGetter.getValueOfAttribut(fileNode, JmenaElementu.DMDID);
+                if (!checkFiles.contains(dmdid)) {
+                    continue;
+                }
+
+                String mimeType = ValuesGetter.getValueOfAttribut(fileNode, "MIMETYPE");
+                if ("application/pdf".equalsIgnoreCase(mimeType)) {
+                    ArrayList<Node> flocatNodes = ValuesGetter.getChildList(fileNode, "mets:FLocat");
+                    for (Node flocatNode : flocatNodes) {
+                        if (!checkPdfA(flocatNode)) {
+                            return false;
+                        }
+                    }
+                }
+            }
 		}
 
         return true;
 
 	}
 
+    private boolean checkPdfA(Node flocatNode) {
+        if (!ValuesGetter.hasAttribut(flocatNode, "xlink:href")) {
+            return nastavChybu("Komponenta neni ve formátu Pdf/A (chybí xlink:href)" + flocatNode,
+                               getMistoChyby(flocatNode));
+        }
+        String href = ValuesGetter.getValueOfAttribut(flocatNode, "xlink:href");
+        // TODO: Dává toto smysl, asi musí být vždy komponenty
+        if (!href.startsWith("komponenty")) {
+            return true;
+        }
+        href = HelperString.edit_path(href);
+        String cestaKeKomponente = SIP_MAIN_helper.getCesta_komponenty(context.getSip())
+                .replaceFirst("komponenty", "") + href;
+
+        File file = new File(cestaKeKomponente);
+        if (file.exists()) {
+            try (PDFAParser parser = Foundries.defaultInstance().createParser(
+                                                                              new FileInputStream(
+                                                                                      file))) {
+                PDFAValidator validator = Foundries.defaultInstance().createValidator(parser
+                        .getFlavour(), false);
+                ValidationResult result = validator.validate(parser);
+                if (!result.isCompliant()) {
+                    return nastavChybu("Komponenta neni ve formátu Pdf/A " + flocatNode,
+                                       getMistoChyby(flocatNode));
+                }
+            } catch (ModelParsingException | EncryptedPdfException | IOException
+                    | ValidationException e) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
