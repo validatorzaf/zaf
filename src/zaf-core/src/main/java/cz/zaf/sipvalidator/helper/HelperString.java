817a2769b01c56a14d3fd7c0daf60a1b5d392e74
Petr Pytelka <petr.pytelka@lightcomp.cz>
2021-07-12 22:21:46 +0200

    Odstraneni nepotrebneho kodu

--- src/zaf-core/src/main/java/cz/zaf/sipvalidator/helper/HelperString.java
+++ src/zaf-core/src/main/java/cz/zaf/sipvalidator/helper/HelperString.java
@@ -35,12 +35,6 @@ public class HelperString {
         }
         return false;
     }
-
-    public static String replace_format_tags(String string){
-        string = string.replaceAll("\n", "");
-        string = string.replaceAll("\t", ""); 
-        return string;
-    }
     
     // upraví / a \ v cestě vždy na path separator.
     public static String replaceSeparators(String path){
@@ -54,35 +48,4 @@ public class HelperString {
         
         return s;
     }
-
-    public static boolean has_string_more_separators(String string){
-        int count = 0;
-        for(int i = 0; i < string.length()-1; i++){
-            char c = string.charAt(i);
-            if(c == ' ' || c == '-' || c == '_' || c == '\\' || c == '/' || c == '.'){
-                count++;
-                if(count < 1) return true;
-            }
-        }
-        return false;
-    }
-    
-    public static boolean has_string_two_separators_abreast(String string){
-        for(int i = 1; i < string.length(); i++){
-            char c = string.charAt(i);
-            char before_c = string.charAt(i-1);
-            if(c == ' ' || c == '-' || c == '_' || c == '\\' || c == '/' || c == '.'){
-                if(before_c == ' ' || before_c == '-' || before_c == '_' || before_c == '\\' || before_c == '/' || before_c == '.'){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    
-    public static boolean is_char_separator(char c){
-        boolean b = (c == ' ' || c == '-' || c == '_' || c == '\\' || c == '/' || c == '.');
-        return b;
-    }
-    
 }
