2b6a49b0d396650dd3579d4f4d84cfe921628b73
Petr Pytelka <petr.pytelka@lightcomp.cz>
2022-02-05 03:03:32 +0100

    Refaktorizace pravidla obs9

--- zaf-core/src/main/java/cz/zaf/sipvalidator/nsesss2017/pravidla06/obs00_09/Pravidlo9.java
+++ zaf-core/src/main/java/cz/zaf/sipvalidator/nsesss2017/pravidla06/obs00_09/Pravidlo9.java
@@ -2,11 +2,12 @@ package cz.zaf.sipvalidator.nsesss2017.pravidla06.obs00_09;
 
 import org.w3c.dom.Node;
 
-import cz.zaf.sipvalidator.nsesss2017.K06PravidloBaseOld;
+import cz.zaf.sipvalidator.exceptions.codes.BaseCode;
+import cz.zaf.sipvalidator.nsesss2017.K06PravidloBase;
 import cz.zaf.sipvalidator.nsesss2017.ValuesGetter;
 
 //OBSAHOVÁ č.9 Element <mets:mets> obsahuje atribut xmlns:xlink s hodnotou http://www.w3.org/1999/xlink.
-public class Pravidlo9 extends K06PravidloBaseOld {
+public class Pravidlo9 extends K06PravidloBase {
 
 	static final public String OBS9 = "obs9";
 
@@ -19,18 +20,20 @@ public class Pravidlo9 extends K06PravidloBaseOld {
 	}
 
 	@Override
-	protected boolean kontrolaPravidla() {
+    protected void kontrola() {
 		Node metsMets = metsParser.getMetsRootNode();
 
-        if(!ValuesGetter.hasAttribut(metsMets, "xmlns:xlink")) {
-        	return nastavChybu("Nenalezen atribut xmlns:xlink kořenového elementu <mets:mets>.", metsMets);
-        }
-        
         String hodnota = ValuesGetter.getValueOfAttribut(metsMets, "xmlns:xlink");
-        if (!hodnota.equals("http://www.w3.org/1999/xlink")) {
-        	return nastavChybu("Atribut xmlns:xlink kořenového elementu <mets:mets> neobsahuje hodnotu http://www.w3.org/1999/xlink.", metsMets);
+        if (hodnota == null) {
+            nastavChybu(BaseCode.CHYBI_ATRIBUT, "Nenalezen atribut xmlns:xlink kořenového elementu <mets:mets>.",
+                        metsMets);
         }
-        return true;
+
+        if (!hodnota.equals("http://www.w3.org/1999/xlink")) {
+            nastavChybu(BaseCode.CHYBNA_HODNOTA_ATRIBUTU,
+                        "Atribut xmlns:xlink kořenového elementu <mets:mets> neobsahuje hodnotu http://www.w3.org/1999/xlink.",
+                        metsMets);
+        }
 	}
 
 }
