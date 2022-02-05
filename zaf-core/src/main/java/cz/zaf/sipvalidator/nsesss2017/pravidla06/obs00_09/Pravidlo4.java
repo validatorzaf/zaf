80d742874c9bb6838ed9e05452af5194164eaf53
Petr Pytelka <petr.pytelka@lightcomp.cz>
2022-02-05 03:01:32 +0100

    Refaktorizace pravidla obs4

--- zaf-core/src/main/java/cz/zaf/sipvalidator/nsesss2017/pravidla06/obs00_09/Pravidlo4.java
+++ zaf-core/src/main/java/cz/zaf/sipvalidator/nsesss2017/pravidla06/obs00_09/Pravidlo4.java
@@ -3,11 +3,12 @@ package cz.zaf.sipvalidator.nsesss2017.pravidla06.obs00_09;
 import org.apache.commons.lang3.StringUtils;
 import org.w3c.dom.Node;
 
-import cz.zaf.sipvalidator.nsesss2017.K06PravidloBaseOld;
+import cz.zaf.sipvalidator.exceptions.codes.BaseCode;
+import cz.zaf.sipvalidator.nsesss2017.K06PravidloBase;
 import cz.zaf.sipvalidator.nsesss2017.ValuesGetter;
 
 //OBSAHOVÁ č.4 Element <mets:mets> obsahuje atribut xmlns:xsi s hodnotou http://www.w3.org/2001/XMLSchema-instance.",
-public class Pravidlo4 extends K06PravidloBaseOld {
+public class Pravidlo4 extends K06PravidloBase {
 
 	static final public String OBS4 = "obs4";
 
@@ -20,24 +21,26 @@ public class Pravidlo4 extends K06PravidloBaseOld {
 	}
 
 	@Override
-	protected boolean kontrolaPravidla() {
+    protected void kontrola() {
 		Node metsMets = metsParser.getMetsRootNode();
 
-        if (!ValuesGetter.hasAttribut(metsMets, "xmlns:xsi")) {
-            return nastavChybu("Nenalezen atribut xmlns:xsi kořenového elementu <mets:mets>.", metsMets); 
-        }
-        
         String hod = ValuesGetter.getValueOfAttribut(metsMets, "xmlns:xsi");
+        if (hod == null) {
+            nastavChybu(BaseCode.CHYBI_ATRIBUT, "Nenalezen atribut xmlns:xsi kořenového elementu <mets:mets>.",
+                        metsMets);
+        }
+
         if (StringUtils.isBlank(hod)) {
-        	return nastavChybu("Atribut xmlns:xsi kořenového elementu <mets:mets> má prázdnou hodnotu.", metsMets);
+            nastavChybu(BaseCode.CHYBNA_HODNOTA_ATRIBUTU,
+                        "Atribut xmlns:xsi kořenového elementu <mets:mets> má prázdnou hodnotu.", metsMets);
         }
         
         if (!hod.equals("http://www.w3.org/2001/XMLSchema-instance")) {
-        	return nastavChybu("Atribut xmlns:xsi kořenového elementu <mets:mets> nemá správnou hodnotu. Jeho hodnota je: "
-                        + hod + ".",
+            nastavChybu(BaseCode.CHYBNA_HODNOTA_ATRIBUTU,
+                        "Atribut xmlns:xsi kořenového elementu <mets:mets> nemá správnou hodnotu. Jeho hodnota je: "
+                                + hod + ".",
                         metsMets);
         }
-		return true;
 	}
 
 }
