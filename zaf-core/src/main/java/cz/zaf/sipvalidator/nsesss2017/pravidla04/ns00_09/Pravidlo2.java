622cf9e0487d7ea81ea52cb5b2a8b94de2bc7794
janzeman974 <janzeman974@penguin>
2024-03-26 07:12:24 +0100

    Odstranění nepotřebného xsi_schema_location ze SipInfo #43

--- zaf-core/src/main/java/cz/zaf/sipvalidator/nsesss2017/pravidla04/ns00_09/Pravidlo2.java
+++ zaf-core/src/main/java/cz/zaf/sipvalidator/nsesss2017/pravidla04/ns00_09/Pravidlo2.java
@@ -23,27 +23,23 @@ public class Pravidlo2 extends NsCheckRuleBase {
 
     @Override
     protected void kontrola() {
-        SipInfo file = this.ctx.getContext().getSip();
         boolean stav = false;
         String detailChyby = null;
 
         Node metsMets = this.ctx.getContext().getMetsParser().getMetsRootNode();
         if (metsMets == null) {
             detailChyby = "Datový balíček SIP neobsahuje kořenový element <mets:mets>.";
-            file.set_xsi_schemaLocation("Nenalezen kořenový element.");
             throw new ZafException(BaseCode.CHYBA, detailChyby);
         } else {
             if (ValuesGetter.hasAttribut(metsMets, "xsi:schemaLocation")) 
             {
                 String hodnota = ValuesGetter.getValueOfAttribut(metsMets, "xsi:schemaLocation");
-            	file.set_xsi_schemaLocation(hodnota);
                 if (hodnota.equals("http://www.loc.gov/METS/ http://www.loc.gov/standards/mets/mets.xsd http://www.mvcr.cz/nsesss/v3 http://www.mvcr.cz/nsesss/v3/nsesss.xsd http://nsess.public.cz/erms_trans/v_01_01 TransakcniProtokolNavrh_verze1.7.xsd")
                     == false && 
                     hodnota.equals("http://www.loc.gov/METS/ http://www.loc.gov/standards/mets/mets.xsd http://www.mvcr.cz/nsesss/v3 http://www.mvcr.cz/nsesss/v3/nsesss.xsd http://nsess.public.cz/erms_trans/v_01_01 http://www.mvcr.cz/nsesss/v3/nsesss-TrP.xsd")
                     == false) 
                 {
                 	detailChyby = "Schéma v souboru SIP je špatně uvedeno, hodnota: " + hodnota;
-                    file.set_xsi_schemaLocation(hodnota);
                     throw new ZafException(BaseCode.CHYBA, detailChyby);
                 } 
             } else {
