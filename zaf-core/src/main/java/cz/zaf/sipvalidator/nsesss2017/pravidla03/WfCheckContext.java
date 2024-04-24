aaf3d4dcac582884a06398a3bb9cd6cef49c06f3
Jan Zeman <Jan Zeman@DESKTOP-6RDGSHE>
2024-04-24 22:14:36 +0200

    Pravidlo3 #43

---
+++ zaf-core/src/main/java/cz/zaf/sipvalidator/nsesss2017/pravidla03/WfCheckContext.java
@@ -0,0 +1,16 @@
+package cz.zaf.sipvalidator.nsesss2017.pravidla03;
+
+import cz.zaf.common.validation.RuleEvaluationContext;
+import cz.zaf.sipvalidator.nsesss2017.KontrolaNsess2017Context;
+
+public class WfCheckContext implements RuleEvaluationContext {
+    private KontrolaNsess2017Context kontrolaCtx;
+
+    public WfCheckContext(final KontrolaNsess2017Context kontrolaCtx) {
+        this.kontrolaCtx = kontrolaCtx;
+    }
+
+    public KontrolaNsess2017Context getContext() {
+        return kontrolaCtx;
+    }
+}
