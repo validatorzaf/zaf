a82aa301d2c38a390ed02ecd72fc590dbffe0942
Petr Pytelka <petr.pytelka@lightcomp.cz>
2025-05-14 14:34:04 +0200

    Zaklad UI

---
+++ zaf-ws/src/main/java/cz/zaf/validator/ws/controller/ZafUIController.java
@@ -0,0 +1,67 @@
+package cz.zaf.validator.ws.controller;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+import cz.zaf.api.rest.model.RequestProcessState;
+import cz.zaf.validator.ws.service.ValidationService;
+
+@Controller
+public class ZafUIController {
+
+	@Autowired
+	private ValidationService validationService;
+
+	@GetMapping("/upload")
+	public String showUploadForm() {
+		return "upload";
+	}
+
+
+	@PostMapping("/upload")
+	public String handleFileUpload(@RequestParam("file") MultipartFile file, Model model) {
+		try {
+			// Convert file to byte array
+			byte[] fileBytes = file.getBytes();
+			
+			String valRequestId = validationService.validate(file, false, null, UUID.randomUUID().toString());
+			int counter = 0;
+			do {
+				RequestProcessState rps = validationService.getStatus(valRequestId);
+				if(rps==RequestProcessState.ERROR) {
+					model.addAttribute("message", "File upload failed!");
+					return "upload";					
+				} else 
+				if(rps==RequestProcessState.FINISHED) {
+					break;
+				}
+				Thread.sleep(1000);
+				counter++;
+			} while(counter<1000);
+			
+			// read result
+
+			// Call API endpoint to check if request was fulfilled
+			/*
+			RestTemplate restTemplate = new RestTemplate();
+			ResponseEntity<String> response = restTemplate.postForEntity("http://api-endpoint-url/check", fileBytes,
+					String.class);
+					*/
+
+			// Read back final response
+			//model.addAttribute("response", response.getBody());
+			return "result";
+		} catch (Exception e) {
+			model.addAttribute("message", "File upload failed!");
+			return "upload";
+		}
+	}
+}
